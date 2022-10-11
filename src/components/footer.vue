008c71694b1c321214de43da7ac77aeedd55c4d1
xiaoxiao <3147264966@qq.com>
2022-10-12 00:12:06 +0800

    finish

---
+++ src/components/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <p>© 2022 Wechat: Xiao-X-1114</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+
+</style>
